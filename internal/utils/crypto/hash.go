bb9650849e6eaa01762a613a93060611abeb5ff3
eb3095 <ebennerit@gmail.com>
2026-05-12 16:34:33 -0700

    Fresh

--- internal/utils/crypto/hash.go
+++ internal/utils/crypto/hash.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/togethercomputer/slurm-operator/internal/utils/structutils"
+	"github.com/SlinkyProject/slurm-operator/internal/utils/structutils"
 )
 
 func CheckSum(b []byte) string {
